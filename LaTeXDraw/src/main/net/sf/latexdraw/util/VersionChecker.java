f7a9576cdc03b1efcb8d0763ecc67d0c876f2f44
arno_b <arno_b@users.sourceforge.net>
2013-02-24 20:19:11 +0100

    3a5 release

--- LaTeXDraw/src/main/net/sf/latexdraw/util/VersionChecker.java
+++ LaTeXDraw/src/main/net/sf/latexdraw/util/VersionChecker.java
@@ -38,7 +38,7 @@ public class VersionChecker extends Thread {
 	public final static String VERSION_STABILITY = "alpha 5"; //$NON-NLS-1$
 
 	/** The identifier of the build */
-	public static final String ID_BUILD = "20120505";//$NON-NLS-1$
+	public static final String ID_BUILD = "20130224";//$NON-NLS-1$
 
 	/** To change if update is needed or not. */
 	public static final boolean WITH_UPDATE = true;
