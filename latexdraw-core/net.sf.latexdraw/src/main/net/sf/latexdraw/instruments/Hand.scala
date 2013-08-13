ea28012f1f2fb8ac97f3e1f8f34657b8d2242c18
arnobl <blouin.arno@gmail.com>
2013-08-13 19:27:39 +0200

    fixed: the selection did not work when the rectangle produced by the DnD has no height or width

--- latexdraw-core/net.sf.latexdraw/src/main/net/sf/latexdraw/instruments/Hand.scala
+++ latexdraw-core/net.sf.latexdraw/src/main/net/sf/latexdraw/instruments/Hand.scala
@@ -229,7 +229,7 @@ private sealed class DnD2Select(hand : Hand) extends Link[SelectShapes, DnDWithK
 		val keys = interaction.getKeys
 
 		// Updating the rectangle used for the interim feedback and for the selection of shapes.
-		selectionBorder.setFrame(minX/zoom, minY/zoom, (maxX-minX)/zoom, (maxY-minY)/zoom)
+		selectionBorder.setFrame(minX/zoom, minY/zoom, Math.max((maxX-minX)/zoom, 1), Math.max((maxY-minY)/zoom, 1))
 		// Cleaning the selected shapes in the action.
 		action.setShape(null)
 
