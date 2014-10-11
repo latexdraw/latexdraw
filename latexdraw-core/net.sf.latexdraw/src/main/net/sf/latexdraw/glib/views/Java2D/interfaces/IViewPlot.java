af2197e1622a7322d563d539e80101df47c52a90
arnobl <blouin.arno@gmail.com>
2014-10-11 20:00:27 +0200

    oops

---
+++ latexdraw-core/net.sf.latexdraw/src/main/net/sf/latexdraw/glib/views/Java2D/interfaces/IViewPlot.java
@@ -0,0 +1,23 @@
+package net.sf.latexdraw.glib.views.Java2D.interfaces;
+
+/**
+ * This interface defines a view of a plot.<br>
+ * <br>
+ * This file is part of LaTeXDraw.<br>
+ * Copyright (c) 2005-2014 Arnaud BLOUIN<br>
+ * <br>
+ * LaTeXDraw is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later version.
+ * <br>
+ * LaTeXDraw is distributed without any warranty; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.<br>
+ * <br>
+ * 2014-10-11br>
+ * @author Arnaud BLOUIN
+ * @since 3.3
+ */
+public interface IViewPlot extends IViewShape {
+	// Nothing more.
+}
