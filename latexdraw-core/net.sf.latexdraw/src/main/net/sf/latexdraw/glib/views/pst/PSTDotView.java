d50ff2cf68c7068173af27317e66fdb86427fed5
arnobl <blouin.arno@gmail.com>
2014-11-02 14:12:29 +0100

    fixed: dots may not be coloured as expected when exported as a PDF document.

--- latexdraw-core/net.sf.latexdraw/src/main/net/sf/latexdraw/glib/views/pst/PSTDotView.java
+++ latexdraw-core/net.sf.latexdraw/src/main/net/sf/latexdraw/glib/views/pst/PSTDotView.java
@@ -57,6 +57,8 @@ class PSTDotView extends PSTClassicalView<IDot> {
 
 		cache.append("\\psdots["); //$NON-NLS-1$
 		cache.append(params);
+		if(shape.isFillable())
+			cache.append(", fillcolor=").append(getColourName(shape.getFillingCol()));
 		cache.append(']').append('(');
 		cache.append((float)LNumber.getCutNumber(x/ppc)).append(',');
 		cache.append((float)LNumber.getCutNumber(y/ppc)).append(')');
