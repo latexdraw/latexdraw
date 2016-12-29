7eaefeeb41ecc3384f59052666bd3d0346911f30
arnobl <blouin.arno@gmail.com>
2016-12-29 13:54:56 +0100

    Report bug URL changed

--- latexdraw-core/net.sf.latexdraw/src/main/net/sf/latexdraw/instruments/Helper.java
+++ latexdraw-core/net.sf.latexdraw/src/main/net/sf/latexdraw/instruments/Helper.java
@@ -94,7 +94,7 @@ public class Helper extends WidgetInstrument {
 		try{
 			addInteractor(new MenuItem2AboutFrame(this, aboutFrame, aboutItem));
 			addInteractor(new MenuItem2ShortcutFrame(this, aboutFrame, shortcutItem));
-			addInteractor(new MenuItem2OpenWebPageInteractor(this, reportBugItem, new URI("https://sourceforge.net/p/latexdraw/bugs/?source=navbar"))); //$NON-NLS-1$
+			addInteractor(new MenuItem2OpenWebPageInteractor(this, reportBugItem, new URI("https://github.com/arnobl/latexdraw/wiki/Manual#how-to-report-a-bug"))); //$NON-NLS-1$
 			addInteractor(new MenuItem2OpenWebPageInteractor(this, forumItem, new URI("https://sourceforge.net/p/latexdraw/discussion/"))); //$NON-NLS-1$
 			addInteractor(new MenuItem2OpenWebPageInteractor(this, donateItem, new URI("http://sourceforge.net/project/project_donations.php?group_id=156523"))); //$NON-NLS-1$
 			addInteractor(new MenuItem2OpenWebPageInteractor(this, manuelItem, new URI("https://github.com/arnobl/latexdraw/wiki/Manual"))); //$NON-NLS-1$
