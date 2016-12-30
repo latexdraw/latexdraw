ce0e592ebee29e5e19f70af87f99cfb9ae6b470a
arnobl <blouin.arno@gmail.com>
2016-12-30 11:29:47 +0100

    3.3.5 released

--- latexdraw-core/net.sf.latexdraw/src/main/net/sf/latexdraw/util/VersionChecker.java
+++ latexdraw-core/net.sf.latexdraw/src/main/net/sf/latexdraw/util/VersionChecker.java
@@ -41,7 +41,7 @@ public class VersionChecker extends Thread {
 	public static final String VERSION_STABILITY = ""; //$NON-NLS-1$
 
 	/** The identifier of the build */
-	public static final String ID_BUILD = "20161022";//$NON-NLS-1$
+	public static final String ID_BUILD = "20161230";//$NON-NLS-1$
 
 	/** To change if update is needed or not. */
 	public static final boolean WITH_UPDATE = true;
