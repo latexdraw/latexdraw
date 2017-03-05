2d18c7ad44eb1ddc246f7b270c19bbbd083ab5dc
arnobl <blouin.arno@gmail.com>
2017-03-05 19:11:29 +0100

    port(instrument): drawing prop customiser ported to JFX
    Signed-off-by: arnobl <blouin.arno@gmail.com>

--- latexdraw-core/net.sf.latexdraw/src/main/net/sf/latexdraw/instruments/DrawingPropertiesCustomiser.java
+++ latexdraw-core/net.sf.latexdraw/src/main/net/sf/latexdraw/instruments/DrawingPropertiesCustomiser.java
@@ -1,3 +1,14 @@
+/*
+ * This file is part of LaTeXDraw
+ * Copyright (c) 2005-2017 Arnaud BLOUIN
+ *  LaTeXDraw is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *  LaTeXDraw is distributed without any warranty; without even the
+ *  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE. See the GNU General Public License for more details.
+ */
 package net.sf.latexdraw.instruments;
 
 import com.google.inject.Inject;
@@ -9,47 +20,43 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
+import net.sf.latexdraw.actions.LatexProperties;
+import net.sf.latexdraw.actions.ModifyLatexProperties;
 import net.sf.latexdraw.util.LNamespace;
 import net.sf.latexdraw.util.LPath;
 import net.sf.latexdraw.view.latex.LaTeXGenerator;
 import net.sf.latexdraw.view.latex.VerticalPosition;
 import org.malai.javafx.instrument.JfxInstrument;
+import org.malai.javafx.instrument.JfxInteractor;
+import org.malai.javafx.instrument.library.CheckboxInteractor;
+import org.malai.javafx.instrument.library.ComboBoxInteractor;
+import org.malai.javafx.instrument.library.SpinnerInteractor;
+import org.malai.javafx.interaction.library.TextChanged;
 import org.malai.undo.Undoable;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 /**
  * This instrument modifies the properties of the drawing.
- * This file is part of LaTeXDraw.
- * Copyright (c) 2005-2017 Arnaud BLOUIN
- * LaTeXDraw is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- * LaTeXDraw is distributed without any warranty; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- * @author Arnaud BLOUIN
- * @since 3.0
  */
 public class DrawingPropertiesCustomiser extends JfxInstrument implements Initializable {
 	/** The field that changes the title of the drawing. */
-	@FXML protected TextField titleField;
+	@FXML private TextField titleField;
 
 	/** The field that changes the label of the drawing. */
-	@FXML protected TextField labelField;
+	@FXML private TextField labelField;
 
 	/** Defines if the horizontal position of the drawing must be centred. */
-	@FXML protected CheckBox middleHorizPosCB;
+	@FXML private CheckBox middleHorizPosCB;
 
 	/** Defines the position of the drawing. */
-	@FXML protected ComboBox<VerticalPosition> positionCB;
+	@FXML private ComboBox<VerticalPosition> positionCB;
 
 	/** Defines the scale of the drawing. */
-	@FXML protected Spinner<Double> scaleField;
+	@FXML private Spinner<Double> scaleField;
 
 	/** The LaTeX code generator. */
-	@Inject protected LaTeXGenerator latexGen;
+	@Inject private LaTeXGenerator latexGen;
 
 	/**
 	 * Creates the instrument.
@@ -61,6 +68,7 @@ public class DrawingPropertiesCustomiser extends JfxInstrument implements Initia
 	@Override
 	public void initialize(final URL location, final ResourceBundle resources) {
 		ShapePropertyCustomiser.scrollOnSpinner(scaleField);
+		setActivated(true);
 	}
 
 	@Override
@@ -89,28 +97,27 @@ public class DrawingPropertiesCustomiser extends JfxInstrument implements Initia
 	public void save(final boolean generalPreferences, final String nsURI, final Document document, final Element root) {
 		super.save(generalPreferences, nsURI, document, root);
 
-		if(document==null||root==null||generalPreferences)
-			return;
+		if(document == null || root == null || generalPreferences) return;
 
 		final String ns = LPath.INSTANCE.getNormaliseNamespaceURI(nsURI);
 
 		if(!latexGen.getCaption().isEmpty()) {
-			Element elt = document.createElement(ns+LNamespace.XML_CAPTION);
+			final Element elt = document.createElement(ns + LNamespace.XML_CAPTION);
 			elt.appendChild(document.createTextNode(latexGen.getCaption()));
 			root.appendChild(elt);
 		}
 		if(!latexGen.getLabel().isEmpty()) {
-			Element elt = document.createElement(ns+LNamespace.XML_LABEL);
+			final Element elt = document.createElement(ns + LNamespace.XML_LABEL);
 			elt.appendChild(document.createTextNode(latexGen.getLabel()));
 			root.appendChild(elt);
 		}
 		if(latexGen.isPositionHoriCentre()) {
-			Element elt = document.createElement(ns+LNamespace.XML_POSITION_HORIZ);
+			final Element elt = document.createElement(ns + LNamespace.XML_POSITION_HORIZ);
 			elt.setTextContent(String.valueOf(latexGen.isPositionHoriCentre()));
 			root.appendChild(elt);
 		}
-		if(latexGen.getPositionVertToken()!=VerticalPosition.NONE) {
-			Element elt = document.createElement(ns+LNamespace.XML_POSITION_VERT);
+		if(latexGen.getPositionVertToken() != VerticalPosition.NONE) {
+			final Element elt = document.createElement(ns + LNamespace.XML_POSITION_VERT);
 			elt.setTextContent(latexGen.getPositionVertToken().toString());
 			root.appendChild(elt);
 		}
@@ -122,12 +129,9 @@ public class DrawingPropertiesCustomiser extends JfxInstrument implements Initia
 
 		final String name = root.getNodeName();
 
-		if(name.endsWith(LNamespace.XML_CAPTION))
-			latexGen.setCaption(root.getTextContent());
-		else if(name.endsWith(LNamespace.XML_LABEL))
-			latexGen.setLabel(root.getTextContent());
-		else if(name.endsWith(LNamespace.XML_POSITION_HORIZ))
-			latexGen.setPositionHoriCentre(Boolean.parseBoolean(root.getTextContent()));
+		if(name.endsWith(LNamespace.XML_CAPTION)) latexGen.setCaption(root.getTextContent());
+		else if(name.endsWith(LNamespace.XML_LABEL)) latexGen.setLabel(root.getTextContent());
+		else if(name.endsWith(LNamespace.XML_POSITION_HORIZ)) latexGen.setPositionHoriCentre(Boolean.parseBoolean(root.getTextContent()));
 		else if(name.endsWith(LNamespace.XML_POSITION_VERT))
 			latexGen.setPositionVertToken(VerticalPosition.getPosition(root.getTextContent()));
 	}
@@ -143,129 +147,76 @@ public class DrawingPropertiesCustomiser extends JfxInstrument implements Initia
 	@Override
 	public void setActivated(final boolean isactivated) {
 		super.setActivated(isactivated);
-
-		if(isactivated)
+		if(isactivated) {
 			updateWidgets();
+		}
 	}
 
 	@Override
-	protected void initialiseInteractors() {
-		// addInteractor(new TextField2CustDrawing(this));
-		// addInteractor(new CheckBox2CustDrawing(this));
-		// addInteractor(new ComboBox2CustDrawing(this));
-		// addInteractor(new Spinner2CustDrawing(this));
+	protected void initialiseInteractors() throws IllegalAccessException, InstantiationException {
+		addInteractor(new TextField2CustDrawing());
+		addInteractor(new CheckBox2CustDrawing());
+		addInteractor(new ComboBox2CustDrawing());
+		addInteractor(new Spinner2CustDrawing());
 	}
 
-	// /** The link that maps a combo box to action that modifies the drawing's
-	// properties. */
-	// protected static class Spinner2CustDrawing extends
-	// InteractorImpl<ModifyLatexProperties, SpinnerModified,
-	// DrawingPropertiesCustomiser> {
-	// protected Spinner2CustDrawing(final DrawingPropertiesCustomiser ins)
-	// throws InstantiationException, IllegalAccessException {
-	// super(ins, false, ModifyLatexProperties.class, SpinnerModified.class);
-	// }
-	//
-	// @Override
-	// public void initAction() {
-	// action.setProperty(LatexProperties.SCALE);
-	// action.setGenerator(instrument.latexGen);
-	// }
-	//
-	// @Override
-	// public void updateAction() {
-	// action.setValue(instrument.scaleField.getValue());
-	// }
-	//
-	// @Override
-	// public boolean isConditionRespected() {
-	// return interaction.getSpinner()==instrument.scaleField;
-	// }
-	// }
-	//
-	//
-	// /** The link that maps a combo box to action that modifies the drawing's
-	// properties. */
-	// protected static class ComboBox2CustDrawing extends
-	// InteractorImpl<ModifyLatexProperties, ListSelectionModified,
-	// DrawingPropertiesCustomiser> {
-	// protected ComboBox2CustDrawing(final DrawingPropertiesCustomiser ins)
-	// throws InstantiationException, IllegalAccessException {
-	// super(ins, false, ModifyLatexProperties.class,
-	// ListSelectionModified.class);
-	// }
-	//
-	// @Override
-	// public void initAction() {
-	// action.setProperty(LatexProperties.POSITION_VERTICAL);
-	// action.setGenerator(instrument.latexGen);
-	// action.setValue(instrument.positionCB.getSelectedItem());
-	// }
-	//
-	// @Override
-	// public boolean isConditionRespected() {
-	// return interaction.getList()==instrument.positionCB;
-	// }
-	// }
-	//
-	//
-	// /** The link that maps a check box to action that modifies the drawing's
-	// properties. */
-	// protected static class CheckBox2CustDrawing extends
-	// InteractorImpl<ModifyLatexProperties, CheckBoxModified,
-	// DrawingPropertiesCustomiser> {
-	// protected CheckBox2CustDrawing(final DrawingPropertiesCustomiser ins)
-	// throws InstantiationException, IllegalAccessException {
-	// super(ins, false, ModifyLatexProperties.class, CheckBoxModified.class);
-	// }
-	//
-	//
-	// @Override
-	// public void initAction() {
-	// action.setProperty(LatexProperties.POSITION_HORIZONTAL);
-	// action.setGenerator(instrument.latexGen);
-	// action.setValue(instrument.middleHorizPosCB.isSelected());
-	// }
-	//
-	// @Override
-	// public boolean isConditionRespected() {
-	// return interaction.getCheckBox()==instrument.middleHorizPosCB;
-	// }
-	// }
-	//
-	//
-	// /** The link that maps text fields to action that modifies the drawing's
-	// properties. */
-	// protected static class TextField2CustDrawing extends
-	// InteractorImpl<ModifyLatexProperties, TextChanged,
-	// DrawingPropertiesCustomiser> {
-	// protected TextField2CustDrawing(final DrawingPropertiesCustomiser ins)
-	// throws InstantiationException, IllegalAccessException {
-	// super(ins, false, ModifyLatexProperties.class, TextChanged.class);
-	// }
-	//
-	// @Override
-	// public void initAction() {
-	// if(interaction.getTextComp()==instrument.labelField)
-	// action.setProperty(LatexProperties.LABEL);
-	// else
-	// action.setProperty(LatexProperties.CAPTION);
-	//
-	// action.setGenerator(instrument.latexGen);
-	// }
-	//
-	//
-	// @Override
-	// public void updateAction() {
-	// super.updateAction();
-	// action.setValue(interaction.getText());
-	// }
-	//
-	//
-	// @Override
-	// public boolean isConditionRespected() {
-	// return interaction.getTextComp()==instrument.labelField ||
-	// interaction.getTextComp()==instrument.titleField;
-	// }
-	// }
+	private class CheckBox2CustDrawing extends CheckboxInteractor<ModifyLatexProperties, DrawingPropertiesCustomiser> {
+		CheckBox2CustDrawing() throws InstantiationException, IllegalAccessException {
+			super(DrawingPropertiesCustomiser.this, ModifyLatexProperties.class, middleHorizPosCB);
+		}
+
+		@Override
+		public void initAction() {
+			action.setProperty(LatexProperties.POSITION_HORIZONTAL);
+			action.setGenerator(instrument.latexGen);
+			action.setValue(instrument.middleHorizPosCB.isSelected());
+		}
+	}
+
+	private class ComboBox2CustDrawing extends ComboBoxInteractor<ModifyLatexProperties, DrawingPropertiesCustomiser> {
+		ComboBox2CustDrawing() throws InstantiationException, IllegalAccessException {
+			super(DrawingPropertiesCustomiser.this, ModifyLatexProperties.class, positionCB);
+		}
+
+		@Override
+		public void initAction() {
+			action.setProperty(LatexProperties.POSITION_VERTICAL);
+			action.setGenerator(instrument.latexGen);
+			action.setValue(instrument.positionCB.getSelectionModel().getSelectedItem());
+		}
+	}
+
+	private class Spinner2CustDrawing extends SpinnerInteractor<ModifyLatexProperties, DrawingPropertiesCustomiser> {
+		Spinner2CustDrawing() throws InstantiationException, IllegalAccessException {
+			super(DrawingPropertiesCustomiser.this, ModifyLatexProperties.class, scaleField);
+		}
+
+		@Override
+		public void initAction() {
+			action.setProperty(LatexProperties.SCALE);
+			action.setGenerator(instrument.latexGen);
+		}
+
+		@Override
+		public void updateAction() {
+			action.setValue(instrument.scaleField.getValue());
+		}
+	}
+
+	private class TextField2CustDrawing extends JfxInteractor<ModifyLatexProperties, TextChanged, DrawingPropertiesCustomiser> {
+		TextField2CustDrawing() throws InstantiationException, IllegalAccessException {
+			super(DrawingPropertiesCustomiser.this, false, ModifyLatexProperties.class, TextChanged.class, labelField, titleField);
+		}
+
+		@Override
+		public void initAction() {
+			action.setProperty(interaction.getWidget() == instrument.labelField ? LatexProperties.LABEL : LatexProperties.CAPTION);
+			action.setGenerator(instrument.latexGen);
+		}
+
+		@Override
+		public void updateAction() {
+			action.setValue(interaction.getWidget().getText());
+		}
+	}
 }
