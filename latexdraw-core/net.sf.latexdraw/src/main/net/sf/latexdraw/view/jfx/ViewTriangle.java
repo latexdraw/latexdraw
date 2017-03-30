13a7b30538ec0f69373ec5a65da449f847f36805
arnobl <blouin.arno@gmail.com>
2017-03-30 21:12:41 +0200

    fixed(view-jfx): triangle's shadow was not created

--- latexdraw-core/net.sf.latexdraw/src/main/net/sf/latexdraw/view/jfx/ViewTriangle.java
+++ latexdraw-core/net.sf.latexdraw/src/main/net/sf/latexdraw/view/jfx/ViewTriangle.java
@@ -1,49 +1,47 @@
 package net.sf.latexdraw.view.jfx;
 
 import javafx.beans.binding.Bindings;
-import javafx.scene.shape.ClosePath;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 import net.sf.latexdraw.models.interfaces.shape.ITriangle;
 
 public class ViewTriangle extends ViewPathShape<ITriangle> {
-	final MoveTo moveTo;
-	final LineTo lineTo1;
-	final LineTo lineTo2;
-
 	/**
 	 * Creates the view.
 	 * @param sh The model.
 	 */
 	ViewTriangle(final ITriangle sh) {
 		super(sh);
-		moveTo = new MoveTo();
+		setupPath(border);
+		setupPath(shadow);
+	}
 
-		moveTo.xProperty().bind(Bindings.createDoubleBinding(() -> sh.getPtAt(0).getX() + sh.getWidth() / 2.0, sh.getPtAt(0).xProperty(), sh.getPtAt(1).xProperty()));
-		moveTo.yProperty().bind(sh.getPtAt(0).yProperty());
-		border.getElements().add(moveTo);
+	private void setupPath(final Path path) {
+		MoveTo moveTo = ViewFactory.INSTANCE.createMoveTo(0d, 0d);
 
-		lineTo1 = new LineTo();
-		lineTo1.xProperty().bind(sh.getPtAt(2).xProperty());
-		lineTo1.yProperty().bind(sh.getPtAt(2).yProperty());
-		border.getElements().add(lineTo1);
+		moveTo.xProperty().bind(Bindings.createDoubleBinding(() -> model.getPtAt(0).getX() + model.getWidth() / 2d,
+			model.getPtAt(0).xProperty(), model.getPtAt(1).xProperty()));
+		moveTo.yProperty().bind(model.getPtAt(0).yProperty());
+		path.getElements().add(moveTo);
 
-		lineTo2 = new LineTo();
-		lineTo2.xProperty().bind(sh.getPtAt(3).xProperty());
-		lineTo2.yProperty().bind(sh.getPtAt(3).yProperty());
-		border.getElements().add(lineTo2);
+		LineTo lineTo = ViewFactory.INSTANCE.createLineTo(0d, 0d);
+		lineTo.xProperty().bind(model.getPtAt(2).xProperty());
+		lineTo.yProperty().bind(model.getPtAt(2).yProperty());
+		path.getElements().add(lineTo);
 
-		border.getElements().add(new ClosePath());
+		lineTo = ViewFactory.INSTANCE.createLineTo(0d, 0d);
+		lineTo.xProperty().bind(model.getPtAt(3).xProperty());
+		lineTo.yProperty().bind(model.getPtAt(3).yProperty());
+		path.getElements().add(lineTo);
+
+		path.getElements().add(ViewFactory.INSTANCE.createClosePath());
 	}
 
 	@Override
 	public void flush() {
-		moveTo.xProperty().unbind();
-		moveTo.yProperty().unbind();
-		lineTo1.xProperty().unbind();
-		lineTo1.yProperty().unbind();
-		lineTo2.xProperty().unbind();
-		lineTo2.yProperty().unbind();
+		border.getElements().forEach(elt -> ViewFactory.INSTANCE.flushPathElement(elt));
+		shadow.getElements().forEach(elt -> ViewFactory.INSTANCE.flushPathElement(elt));
 		super.flush();
 	}
 }
---
+++ latexdraw-core/net.sf.latexdraw/src/test/test/views/jfx/TestViewTriangle.java
@@ -0,0 +1,54 @@
+package test.views.jfx;
+
+import java.util.concurrent.TimeoutException;
+import javafx.geometry.Bounds;
+import javafx.scene.shape.Path;
+import net.sf.latexdraw.models.ShapeFactory;
+import net.sf.latexdraw.models.interfaces.shape.ITriangle;
+import net.sf.latexdraw.view.jfx.ViewTriangle;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestViewTriangle extends TestViewBorderedShape<ViewTriangle, ITriangle, Path> {
+	@BeforeClass
+	public static void beforeClass() throws TimeoutException {
+		FxToolkit.registerPrimaryStage();
+	}
+
+	@Override
+	protected ITriangle createModel() {
+		final ITriangle sh = ShapeFactory.INST.createTriangle();
+		sh.setWidth(11d);
+		sh.setHeight(21d);
+		sh.setX(103d);
+		sh.setY(207d);
+		return sh;
+	}
+
+	@Test
+	@Override
+	public void testOnTranslateX() {
+		final Bounds before = view.getBorder().getBoundsInParent();
+		model.translate(17d, 0d);
+		view.getBorder().setTranslateX(-17d);
+		assertEquals(before, view.getBorder().getBoundsInParent());
+	}
+
+	@Test
+	@Override
+	public void testOnTranslateY() {
+		final Bounds before = view.getBorder().getBoundsInParent();
+		model.translate(0d, -19d);
+		view.getBorder().setTranslateY(19d);
+		assertEquals(before, view.getBorder().getBoundsInParent());
+	}
+
+	@Test
+	@Override
+	public void testShadowPositionSameThanBorder() {
+		assertEquals(view.getBorder().getElements(), view.getShadow().get().getElements());
+	}
+}
