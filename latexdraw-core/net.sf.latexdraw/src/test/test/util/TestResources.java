8aaae8f481c665f9693390f213d52e0ffee18729
arnobl <blouin.arno@gmail.com>
2017-04-20 20:18:12 +0200

    refactor(test): better test writing and no more swing

--- latexdraw-core/net.sf.latexdraw/src/test/test/util/TestResources.java
+++ latexdraw-core/net.sf.latexdraw/src/test/test/util/TestResources.java
@@ -1,144 +1,142 @@
 package test.util;
 
-import static org.junit.Assert.assertNotNull;
-
-import java.net.URL;
-
-import javax.swing.ImageIcon;
-
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
 public class TestResources {
-	@Test
-	public void testRessources() {
-		assertNotNull(loadImageIcon("/res/LaTeXDrawIcon.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/LaTeXDrawSmall.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/emblem-important.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/help-browser.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/comment.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/preferences-desktop-theme.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/New.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/hatch/hatch.none.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/hatch/hatch.vert.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/hatch/hatch.horiz.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/hatch/hatch.cross.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.arc.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.arc.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.arc.r.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.arc.r.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.arrow.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.arrow.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.barEnd.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.barEnd.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.barIn.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.barIn.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.bracket.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.bracket.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.bracket.r.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.bracket.r.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.circle.end.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.circle.end.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.circle.in.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.circle.in.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.dbleArrow.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.dbleArrow.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.disk.end.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.disk.end.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.disk.in.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.disk.in.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.none.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.none.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.rarrow.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.rarrow.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.rdbleArrow.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.rdbleArrow.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.roundIn.left.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/arrowStyles/line.roundIn.right.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.asterisk.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.none.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.bar.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.o.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.cross.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.diamond.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.diamondF.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.ocross.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.oplus.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.pentagon.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.pentagonF.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.plus.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.square.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.squareF.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.triangle.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dotStyles/dot.triangleF.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/lineStyles/lineStyle.none.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/lineStyles/lineStyle.dashed.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/lineStyles/lineStyle.dotted.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/doubleBoundary/double.boundary.middle.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/doubleBoundary/double.boundary.into.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/doubleBoundary/double.boundary.out.png"));//$NON-NLS-1$
 
-		assertNotNull(loadImageIcon("/res/grid.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/Copy.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/Paste.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/Cut.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/Draw.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/text.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/polygon.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/background.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/foreground.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/Undo.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/Redo.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/Magnify.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/wedge.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/Arc.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/document-open.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/document-save.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/document-save-as.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/circle.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/del.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/dot.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/ellipse.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/empty.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/rectangle.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/rhombus.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/select.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/square.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/triangle.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/join.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/separate.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/bezierCurve.png"));//$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/chord.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/joinedLines.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/emblem-unreadable.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/stop.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/image-x-generic.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/thickness.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/hatch/gradient.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/rotation180.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/rotation90.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/rotation270.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/mirrorH.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/mirrorV.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/axes.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/texEditor.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/freehand/curve.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/freehand/line.png")); //$NON-NLS-1$
-		assertNotNull(loadImageIcon("/res/closedBezier.png")); //$NON-NLS-1$
+	private String path;
+
+	public TestResources(final String pathToTest) {
+		path = pathToTest;
 	}
 
-	public ImageIcon loadImageIcon(String path) {
-		URL url;
+	@Parameterized.Parameters
+	public static Collection<String> getPaths() {
+		return Arrays.asList("/res/LaTeXDrawIcon.png",
+			"/res/LaTeXDrawSmall.png",
+			"/res/emblem-important.png",
+			"/res/help-browser.png",
+			"/res/comment.png",
+			"/res/preferences-desktop-theme.png",
+			"/res/New.png",
+			"/res/hatch/hatch.none.png",
+			"/res/hatch/hatch.vert.png",
+			"/res/hatch/hatch.horiz.png",
+			"/res/hatch/hatch.cross.png",
+			"/res/arrowStyles/line.arc.left.png",
+			"/res/arrowStyles/line.arc.right.png",
+			"/res/arrowStyles/line.arc.r.left.png",
+			"/res/arrowStyles/line.arc.r.right.png",
+			"/res/arrowStyles/line.arrow.left.png",
+			"/res/arrowStyles/line.arrow.right.png",
+			"/res/arrowStyles/line.barEnd.left.png",
+			"/res/arrowStyles/line.barEnd.right.png",
+			"/res/arrowStyles/line.barIn.left.png",
+			"/res/arrowStyles/line.barIn.right.png",
+			"/res/arrowStyles/line.bracket.left.png",
+			"/res/arrowStyles/line.bracket.right.png",
+			"/res/arrowStyles/line.bracket.r.left.png",
+			"/res/arrowStyles/line.bracket.r.right.png",
+			"/res/arrowStyles/line.circle.end.left.png",
+			"/res/arrowStyles/line.circle.end.right.png",
+			"/res/arrowStyles/line.circle.in.left.png",
+			"/res/arrowStyles/line.circle.in.right.png",
+			"/res/arrowStyles/line.dbleArrow.left.png",
+			"/res/arrowStyles/line.dbleArrow.right.png",
+			"/res/arrowStyles/line.disk.end.left.png",
+			"/res/arrowStyles/line.disk.end.right.png",
+			"/res/arrowStyles/line.disk.in.left.png",
+			"/res/arrowStyles/line.disk.in.right.png",
+			"/res/arrowStyles/line.none.left.png",
+			"/res/arrowStyles/line.none.right.png",
+			"/res/arrowStyles/line.rarrow.left.png",
+			"/res/arrowStyles/line.rarrow.right.png",
+			"/res/arrowStyles/line.rdbleArrow.left.png",
+			"/res/arrowStyles/line.rdbleArrow.right.png",
+			"/res/arrowStyles/line.roundIn.left.png",
+			"/res/arrowStyles/line.roundIn.right.png",
+			"/res/dotStyles/dot.asterisk.png",
+			"/res/dotStyles/dot.none.png",
+			"/res/dotStyles/dot.bar.png",
+			"/res/dotStyles/dot.o.png",
+			"/res/dotStyles/dot.cross.png",
+			"/res/dotStyles/dot.diamond.png",
+			"/res/dotStyles/dot.diamondF.png",
+			"/res/dotStyles/dot.ocross.png",
+			"/res/dotStyles/dot.oplus.png",
+			"/res/dotStyles/dot.pentagon.png",
+			"/res/dotStyles/dot.pentagonF.png",
+			"/res/dotStyles/dot.plus.png",
+			"/res/dotStyles/dot.square.png",
+			"/res/dotStyles/dot.squareF.png",
+			"/res/dotStyles/dot.triangle.png",
+			"/res/dotStyles/dot.triangleF.png",
+			"/res/lineStyles/lineStyle.none.png",
+			"/res/lineStyles/lineStyle.dashed.png",
+			"/res/lineStyles/lineStyle.dotted.png",
+			"/res/doubleBoundary/double.boundary.middle.png",
+			"/res/doubleBoundary/double.boundary.into.png",
+			"/res/doubleBoundary/double.boundary.out.png",
+			"/res/grid.png",
+			"/res/Copy.png",
+			"/res/Paste.png",
+			"/res/Cut.png",
+			"/res/Draw.png",
+			"/res/text.png",
+			"/res/polygon.png",
+			"/res/background.png",
+			"/res/foreground.png",
+			"/res/Undo.png",
+			"/res/Redo.png",
+			"/res/Magnify.png",
+			"/res/wedge.png",
+			"/res/Arc.png",
+			"/res/document-open.png",
+			"/res/document-save.png",
+			"/res/document-save-as.png",
+			"/res/circle.png",
+			"/res/del.png",
+			"/res/dot.png",
+			"/res/ellipse.png",
+			"/res/empty.png",
+			"/res/rectangle.png",
+			"/res/rhombus.png",
+			"/res/select.png",
+			"/res/square.png",
+			"/res/triangle.png",
+			"/res/join.png",
+			"/res/separate.png",
+			"/res/bezierCurve.png",
+			"/res/chord.png",
+			"/res/joinedLines.png",
+			"/res/emblem-unreadable.png",
+			"/res/stop.png",
+			"/res/image-x-generic.png",
+			"/res/thickness.png",
+			"/res/hatch/gradient.png",
+			"/res/rotation180.png",
+			"/res/rotation90.png",
+			"/res/rotation270.png",
+			"/res/mirrorH.png",
+			"/res/mirrorV.png",
+			"/res/axes.png",
+			"/res/texEditor.png",
+			"/res/freehand/curve.png",
+			"/res/freehand/line.png",
+			"/res/closedBezier.png");
+	}
 
-		try {
-			url = getClass().getResource(path);
-
-			if(url == null)
-				return null;
-
-			return new ImageIcon(url);
-
-		}catch(Exception e) {
-			return null;
-		}
+	@Test
+	public void testFileResource() {
+		assertTrue(Files.isReadable(Paths.get(getClass().getResource(path).getPath())));
 	}
 }
