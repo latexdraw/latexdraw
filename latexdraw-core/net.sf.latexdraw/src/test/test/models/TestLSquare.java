15bb38529e9918d6237ca0a33cdc2ee31583c663
arnobl <blouin.arno@gmail.com>
2017-07-20 22:31:01 +0200

    test(data): use of JUnit theories to factorise tests on lineArc shapes

--- latexdraw-core/net.sf.latexdraw/src/test/test/models/TestLSquare.java
+++ latexdraw-core/net.sf.latexdraw/src/test/test/models/TestLSquare.java
@@ -8,24 +8,57 @@ import net.sf.latexdraw.models.interfaces.shape.IPositionShape;
 import net.sf.latexdraw.models.interfaces.shape.IShape;
 import net.sf.latexdraw.models.interfaces.shape.ISquare;
 import net.sf.latexdraw.models.interfaces.shape.ISquaredShape;
-import org.junit.Before;
 import org.junit.Test;
-import test.models.interfaces.TestISquare;
+import test.HelperTest;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class TestLSquare extends TestISquare<ISquare> {
-	@Before
-	public void setUp() {
-		shape = ShapeFactory.INST.createSquare();
-		shape2 = ShapeFactory.INST.createSquare();
+public class TestLSquare implements HelperTest {
+	@Test
+	public void testConstructorsOKPoints() {
+		ISquare sq = ShapeFactory.INST.createSquare();
+		assertEquals(4, sq.getNbPoints());
 	}
 
-	@Override
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstructorsNotNaN() {
+		ShapeFactory.INST.createSquare(ShapeFactory.INST.createPoint(Double.NaN, 0), 10);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstructorsNotOK0() {
+		ShapeFactory.INST.createSquare(ShapeFactory.INST.createPoint(), 0);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstructorsNotOKNeg() {
+		ShapeFactory.INST.createSquare(ShapeFactory.INST.createPoint(), -10);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstructorsNotOKNaNSize() {
+		ShapeFactory.INST.createSquare(ShapeFactory.INST.createPoint(), Double.NaN);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstructorsNotOKInfSize() {
+		ShapeFactory.INST.createSquare(ShapeFactory.INST.createPoint(), Double.POSITIVE_INFINITY);
+	}
+
+	@Test
+	public void testConstructorsOK() {
+		final ISquare sq = ShapeFactory.INST.createSquare(ShapeFactory.INST.createPoint(20, 26), 11);
+		assertEqualsDouble(20., sq.getPosition().getX());
+		assertEqualsDouble(26., sq.getPosition().getY());
+		assertEqualsDouble(11., sq.getWidth());
+		assertEqualsDouble(11., sq.getHeight());
+	}
+
 	@Test
 	public void testIsTypeOf() {
+		final ISquare shape = ShapeFactory.INST.createSquare();
 		assertFalse(shape.isTypeOf(null));
 		assertFalse(shape.isTypeOf(IGrid.class));
 		assertFalse(shape.isTypeOf(ICircle.class));
@@ -35,45 +68,5 @@ public class TestLSquare extends TestISquare<ISquare> {
 		assertTrue(shape.isTypeOf(ILineArcProp.class));
 		assertTrue(shape.isTypeOf(ISquare.class));
 		assertTrue(shape.isTypeOf(shape.getClass()));
-	}
-
-	@Test
-	public void testConstructorsOKPoints() {
-		ISquare sq = ShapeFactory.INST.createSquare();
-		assertEquals(4, sq.getNbPoints());
-	}
-
-	@Test(expected=IllegalArgumentException.class)
-	public void testConstructorsNotNaN() {
-		ShapeFactory.INST.createSquare(ShapeFactory.INST.createPoint(Double.NaN, 0), 10);
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testConstructorsNotOK0() {
-		ShapeFactory.INST.createSquare(ShapeFactory.INST.createPoint(), 0);
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testConstructorsNotOKNeg() {
-		ShapeFactory.INST.createSquare(ShapeFactory.INST.createPoint(), -10);
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testConstructorsNotOKNaNSize() {
-		ShapeFactory.INST.createSquare(ShapeFactory.INST.createPoint(), Double.NaN);
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void testConstructorsNotOKInfSize() {
-		ShapeFactory.INST.createSquare(ShapeFactory.INST.createPoint(), Double.POSITIVE_INFINITY);
-	}
-	
-	@Test
-	public void testConstructorsOK() {
-		ISquare sq = ShapeFactory.INST.createSquare(ShapeFactory.INST.createPoint(20, 26), 11);
-		assertEqualsDouble(20., sq.getPosition().getX());
-		assertEqualsDouble(26., sq.getPosition().getY());
-		assertEqualsDouble(11., sq.getWidth());
-		assertEqualsDouble(11., sq.getHeight());
 	}
 }
