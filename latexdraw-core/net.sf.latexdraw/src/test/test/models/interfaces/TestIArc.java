40da4892fc7057068e4616549afc376195b7efd7
arnobl <blouin.arno@gmail.com>
2017-07-21 17:37:23 +0200

    test(data): use of JUnit theories

--- latexdraw-core/net.sf.latexdraw/src/test/test/models/interfaces/TestIArc.java
+++ latexdraw-core/net.sf.latexdraw/src/test/test/models/interfaces/TestIArc.java
@@ -1,106 +1,60 @@
 package test.models.interfaces;
 
 import net.sf.latexdraw.models.interfaces.shape.ArcStyle;
-import net.sf.latexdraw.models.interfaces.shape.IArc;
-import org.junit.Test;
+import net.sf.latexdraw.models.interfaces.shape.ICircleArc;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+import test.HelperTest;
+import test.data.ArcData;
+import test.data.DoubleData;
 
 import static org.junit.Assert.assertEquals;
 
-public abstract class TestIArc<T extends IArc> extends TestIPositionShape<T> {
-	@Test
-	public abstract void testGetStartPoint();
-
-	@Test
-	public abstract void testGetEndPoint();
-
-	@Test
-	public void testSetGetStyle() {
-		shape.setArcStyle(ArcStyle.CHORD);
-		assertEquals(ArcStyle.CHORD, shape.getArcStyle());
-
-		shape.setArcStyle(ArcStyle.ARC);
-		assertEquals(ArcStyle.ARC, shape.getArcStyle());
-
-		shape.setArcStyle(ArcStyle.WEDGE);
-		assertEquals(ArcStyle.WEDGE, shape.getArcStyle());
-
-		shape.setArcStyle(null);
-		assertEquals(ArcStyle.WEDGE, shape.getArcStyle());
+@RunWith(Theories.class)
+public class TestIArc implements HelperTest {
+	@Theory
+	public void testSetGetStyle(@ArcData final ICircleArc shape, final ArcStyle style) {
+		shape.setArcStyle(style);
+		assertEquals(style, shape.getArcStyle());
 	}
 
-	@Test
-	public void testSetGetAngleStart() {
-		shape.setAngleStart(Math.PI);
-		assertEqualsDouble(Math.PI, shape.getAngleStart());
-
-		shape.setAngleStart(0.);
-		assertEqualsDouble(0., shape.getAngleStart());
-
-		shape.setAngleStart(2. * Math.PI);
-		assertEqualsDouble(2. * Math.PI, shape.getAngleStart());
-
-		shape.setAngleStart(4. * Math.PI);
-		assertEqualsDouble(4. * Math.PI, shape.getAngleStart());
-
-		shape.setAngleStart(-2. * Math.PI);
-		assertEqualsDouble(-2. * Math.PI, shape.getAngleStart());
-
-		shape.setAngleStart(-4. * Math.PI);
-		assertEqualsDouble(-4. * Math.PI, shape.getAngleStart());
-
-		shape.setAngleStart(Double.NaN);
-		assertEqualsDouble(-4. * Math.PI, shape.getAngleStart());
-
-		shape.setAngleStart(Double.NEGATIVE_INFINITY);
-		assertEqualsDouble(-4. * Math.PI, shape.getAngleStart());
-
-		shape.setAngleStart(Double.POSITIVE_INFINITY);
-		assertEqualsDouble(-4. * Math.PI, shape.getAngleStart());
+	@Theory
+	public void testSetGetAngleStart(@ArcData final ICircleArc shape, @DoubleData final double angle) {
+		shape.setAngleStart(angle);
+		assertEqualsDouble(angle, shape.getAngleStart());
 	}
 
-	@Test
-	public void testSetGetAngleEnd() {
-		shape.setAngleEnd(Math.PI);
-		assertEqualsDouble(Math.PI, shape.getAngleEnd());
-
-		shape.setAngleEnd(0.);
-		assertEqualsDouble(0., shape.getAngleEnd());
-
-		shape.setAngleEnd(2. * Math.PI);
-		assertEqualsDouble(2. * Math.PI, shape.getAngleEnd());
-
-		shape.setAngleEnd(4. * Math.PI);
-		assertEqualsDouble(4. * Math.PI, shape.getAngleEnd());
-
-		shape.setAngleEnd(-2. * Math.PI);
-		assertEqualsDouble(-2. * Math.PI, shape.getAngleEnd());
-
-		shape.setAngleEnd(-4. * Math.PI);
-		assertEqualsDouble(-4. * Math.PI, shape.getAngleEnd());
-
-		shape.setAngleEnd(Double.NaN);
-		assertEqualsDouble(-4. * Math.PI, shape.getAngleEnd());
-
-		shape.setAngleEnd(Double.NEGATIVE_INFINITY);
-		assertEqualsDouble(-4. * Math.PI, shape.getAngleEnd());
-
-		shape.setAngleEnd(Double.POSITIVE_INFINITY);
-		assertEqualsDouble(-4. * Math.PI, shape.getAngleEnd());
+	@Theory
+	public void testSetGetAngleStartKO(@ArcData final ICircleArc shape, @DoubleData(vals = {}, bads = true) final double angle) {
+		shape.setAngleStart(1d);
+		shape.setAngleStart(angle);
+		assertEqualsDouble(1d, shape.getAngleStart());
 	}
 
-	@Override
-	@Test
-	public void testCopy() {
-		super.testCopy();
+	@Theory
+	public void testSetGetAngleEnd(@ArcData final ICircleArc shape, @DoubleData final double angle) {
+		shape.setAngleEnd(angle);
+		assertEqualsDouble(angle, shape.getAngleEnd());
+	}
 
-		shape2.setAngleEnd(-4. * Math.PI);
-		shape2.setAngleStart(-2. * Math.PI);
+	@Theory
+	public void testSetGetAngleEndKO(@ArcData final ICircleArc shape, @DoubleData(vals = {}, bads = true) final double angle) {
+		shape.setAngleEnd(1d);
+		shape.setAngleEnd(angle);
+		assertEqualsDouble(1d, shape.getAngleEnd());
+	}
+
+	@Theory
+	public void testCopy(@ArcData final ICircleArc shape, @ArcData final ICircleArc shape2) {
+		shape2.setAngleEnd(-4d * Math.PI);
+		shape2.setAngleStart(-2d * Math.PI);
 		shape2.setArcStyle(ArcStyle.WEDGE);
 
 		shape.copy(shape2);
 
-		assertEqualsDouble(-4. * Math.PI, shape.getAngleEnd());
-		assertEqualsDouble(-2. * Math.PI, shape.getAngleStart());
+		assertEqualsDouble(-4d * Math.PI, shape.getAngleEnd());
+		assertEqualsDouble(-2d * Math.PI, shape.getAngleStart());
 		assertEquals(ArcStyle.WEDGE, shape.getArcStyle());
 	}
 }
