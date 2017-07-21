e9e944288a5353d9bceced3b4e7600aa21fb212a
arnobl <blouin.arno@gmail.com>
2017-07-21 12:17:03 +0200

    fix(test): double supplier cannot be empty

--- latexdraw-core/net.sf.latexdraw/src/test/test/data/DoubleSupplier.java
+++ latexdraw-core/net.sf.latexdraw/src/test/test/data/DoubleSupplier.java
@@ -11,14 +11,8 @@ import org.junit.experimental.theories.PotentialAssignment;
 public class DoubleSupplier extends ParameterSupplier {
 	@Override
 	public List<PotentialAssignment> getValueSources(final ParameterSignature sig) throws Throwable {
-		DoubleData doubledata = sig.getAnnotation(DoubleData.class);
-		double[] vals = doubledata.vals();
-
-		if(vals.length == 0) {
-			vals = ValuesGenerator.doubleValues;
-		}
-
-		DoubleStream stream = Arrays.stream(vals);
+		final DoubleData doubledata = sig.getAnnotation(DoubleData.class);
+		DoubleStream stream = Arrays.stream(doubledata.vals());
 
 		if(doubledata.bads()) {
 			stream = DoubleStream.concat(stream, DoubleStream.of(Double.NaN, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY));
