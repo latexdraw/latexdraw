5061df0c3302685913582023c27a300c5a2d5ce1
arnobl <blouin.arno@gmail.com>
2017-07-25 12:37:09 +0200

    refactor(test): tests refactored

--- latexdraw-core/net.sf.latexdraw/src/test/test/models/TestGLibUtilities.java
+++ latexdraw-core/net.sf.latexdraw/src/test/test/models/TestGLibUtilities.java
@@ -3,223 +3,44 @@ package test.models;
 import net.sf.latexdraw.models.MathUtils;
 import net.sf.latexdraw.models.ShapeFactory;
 import org.junit.Test;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+import test.data.DoubleData;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+@RunWith(Theories.class)
 public class TestGLibUtilities {
 	@Test
 	public void testIsValidPointNull() {
 		assertFalse(MathUtils.INST.isValidPt(null));
 	}
 
-	@Test
-	public void testIsValidPointNaN0() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(Double.NaN, 0)));
+	@Theory
+	public void testIsValidPointKOX(@DoubleData(vals = {}, bads = true) final double value) {
+		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(value, 0d)));
 	}
 
-	@Test
-	public void testIsValidPointNaNNaN() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(Double.NaN, Double.NaN)));
+	@Theory
+	public void testIsValidPointKOY(@DoubleData(vals = {}, bads = true) final double value) {
+		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(0d, value)));
 	}
 
-	@Test
-	public void testIsValidPoint0NaN() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(0, Double.NaN)));
+	@Theory
+	public void testIsValidPoint(@DoubleData final double x, @DoubleData final double y) {
+		assertTrue(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(x, y)));
 	}
 
-	@Test
-	public void testIsValidPointPOSINF0() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(Double.POSITIVE_INFINITY, 0)));
+
+	@Theory
+	public void testIsValidCoordinateKO(@DoubleData(vals = {}, bads = true) final double value) {
+		assertFalse(MathUtils.INST.isValidCoord(value));
 	}
 
-	@Test
-	public void testIsValidPointPOSINFPOSINF() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY)));
-	}
-
-	@Test
-	public void testIsValidPoint0POSINF() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(0, Double.POSITIVE_INFINITY)));
-	}
-
-	@Test
-	public void testIsValidPointNEGINF0() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(Double.NEGATIVE_INFINITY, 0)));
-	}
-
-	@Test
-	public void testIsValidPointNEGINFNEGINF() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY)));
-	}
-
-	@Test
-	public void testIsValidPoint0NEGINF() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(0, Double.NEGATIVE_INFINITY)));
-	}
-
-	@Test
-	public void testIsValidPointNEGINFPOSINF() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY)));
-	}
-
-	@Test
-	public void testIsValidPointPOSINFNEGINF() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY)));
-	}
-
-	@Test
-	public void testIsValidPointNANNEGINF() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(Double.NaN, Double.NEGATIVE_INFINITY)));
-	}
-
-	@Test
-	public void testIsValidPointNEGINFNAN() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(Double.NEGATIVE_INFINITY, Double.NaN)));
-	}
-
-	@Test
-	public void testIsValidPointNANPOSINF() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(Double.NaN, Double.POSITIVE_INFINITY)));
-	}
-
-	@Test
-	public void testIsValidPointPOSINFNAN() {
-		assertFalse(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(Double.POSITIVE_INFINITY, Double.NaN)));
-	}
-
-	@Test
-	public void testIsValidPoint00() {
-		assertTrue(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(0, 0)));
-	}
-
-	@Test
-	public void testIsValidPointPOSPOS() {
-		assertTrue(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(1000000, 1000000)));
-	}
-
-	@Test
-	public void testIsValidPointNEGNEG() {
-		assertTrue(MathUtils.INST.isValidPt(ShapeFactory.INST.createPoint(-1000000, -1000000)));
-	}
-
-	@Test
-	public void testIsValidPointCoordsNAN0() {
-		assertFalse(MathUtils.INST.isValidPt(Double.NaN, 0));
-	}
-
-	@Test
-	public void testIsValidPointCoordsNANNAN() {
-		assertFalse(MathUtils.INST.isValidPt(Double.NaN, Double.NaN));
-	}
-
-	@Test
-	public void testIsValidPointCoords0NAN() {
-		assertFalse(MathUtils.INST.isValidPt(0, Double.NaN));
-	}
-
-	@Test
-	public void testIsValidPointCoordsPOSINF0() {
-		assertFalse(MathUtils.INST.isValidPt(Double.POSITIVE_INFINITY, 0));
-	}
-
-	@Test
-	public void testIsValidPointCoordsPOSINFPOSINF() {
-		assertFalse(MathUtils.INST.isValidPt(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY));
-	}
-
-	@Test
-	public void testIsValidPointCoords0POSINF() {
-		assertFalse(MathUtils.INST.isValidPt(0, Double.POSITIVE_INFINITY));
-	}
-
-	@Test
-	public void testIsValidPointCoordsNEGINF0() {
-		assertFalse(MathUtils.INST.isValidPt(Double.NEGATIVE_INFINITY, 0));
-	}
-
-	@Test
-	public void testIsValidPointCoordsNEGINFNEGINF() {
-		assertFalse(MathUtils.INST.isValidPt(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY));
-	}
-
-	@Test
-	public void testIsValidPointCoords0NEGINF() {
-		assertFalse(MathUtils.INST.isValidPt(0, Double.NEGATIVE_INFINITY));
-	}
-
-	@Test
-	public void testIsValidPointCoordsNEGINFPOSINF() {
-		assertFalse(MathUtils.INST.isValidPt(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY));
-	}
-
-	@Test
-	public void testIsValidPointCoordsPOSINFNEGINF() {
-		assertFalse(MathUtils.INST.isValidPt(Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY));
-	}
-
-	@Test
-	public void testIsValidPointCoordsNANNEGINF() {
-		assertFalse(MathUtils.INST.isValidPt(Double.NaN, Double.NEGATIVE_INFINITY));
-	}
-
-	@Test
-	public void testIsValidPointCoordsNEGINFNAN() {
-		assertFalse(MathUtils.INST.isValidPt(Double.NEGATIVE_INFINITY, Double.NaN));
-	}
-
-	@Test
-	public void testIsValidPointCoordsNANPOSINF() {
-		assertFalse(MathUtils.INST.isValidPt(Double.NaN, Double.POSITIVE_INFINITY));
-	}
-
-	@Test
-	public void testIsValidPointCoordsPOSINFNAN() {
-		assertFalse(MathUtils.INST.isValidPt(Double.POSITIVE_INFINITY, Double.NaN));
-	}
-
-	@Test
-	public void testIsValidPointCoords00() {
-		assertTrue(MathUtils.INST.isValidPt(0, 0));
-	}
-
-	@Test
-	public void testIsValidPointCoordsPOSPOS() {
-		assertTrue(MathUtils.INST.isValidPt(1000000, 1000000));
-	}
-
-	@Test
-	public void testIsValidPointCoordsNEGNEG() {
-		assertTrue(MathUtils.INST.isValidPt(-1000000, -1000000));
-	}
-
-	@Test
-	public void testIsValidCoordinateNAN() {
-		assertFalse(MathUtils.INST.isValidCoord(Double.NaN));
-	}
-
-	@Test
-	public void testIsValidCoordinatePOSINF() {
-		assertFalse(MathUtils.INST.isValidCoord(Double.POSITIVE_INFINITY));
-	}
-
-	@Test
-	public void testIsValidCoordinateNEGINF() {
-		assertFalse(MathUtils.INST.isValidCoord(Double.NEGATIVE_INFINITY));
-	}
-
-	@Test
-	public void testIsValidCoordinate0() {
-		assertTrue(MathUtils.INST.isValidCoord(0));
-	}
-
-	@Test
-	public void testIsValidCoordinatePOS() {
-		assertTrue(MathUtils.INST.isValidCoord(1000000));
-	}
-
-	@Test
-	public void testIsValidCoordinateNEG() {
-		assertTrue(MathUtils.INST.isValidCoord(-1000000));
+	@Theory
+	public void testIsValidCoordinate(@DoubleData final double value) {
+		assertTrue(MathUtils.INST.isValidCoord(value));
 	}
 }
