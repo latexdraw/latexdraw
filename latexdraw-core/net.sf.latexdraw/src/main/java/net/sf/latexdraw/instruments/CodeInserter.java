291e81c561a16a8999def5a7e139151ace883e99
arnobl <blouin.arno@gmail.com>
2017-10-09 20:29:52 +0200

    fixed(ui): feedback from the code insertion dialogue was missing

--- latexdraw-core/net.sf.latexdraw/src/main/java/net/sf/latexdraw/instruments/CodeInserter.java
+++ latexdraw-core/net.sf.latexdraw/src/main/java/net/sf/latexdraw/instruments/CodeInserter.java
@@ -13,6 +13,8 @@ package net.sf.latexdraw.instruments;
 import com.google.inject.Inject;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -21,14 +23,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.web.WebView;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import net.sf.latexdraw.LaTeXDraw;
 import net.sf.latexdraw.actions.InsertPSTCode;
 import net.sf.latexdraw.badaboom.BadaboomCollector;
 import net.sf.latexdraw.models.interfaces.shape.IDrawing;
+import net.sf.latexdraw.parsers.pst.PSTContext;
+import net.sf.latexdraw.parsers.pst.PSTLatexdrawListener;
+import net.sf.latexdraw.parsers.pst.PSTLexer;
+import net.sf.latexdraw.parsers.pst.PSTParser;
+import net.sf.latexdraw.util.LSystem;
 import net.sf.latexdraw.util.LangTool;
+import org.antlr.v4.runtime.ANTLRErrorListener;
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.tree.ErrorNode;
 import org.malai.action.library.InactivateInstrument;
 import org.malai.javafx.instrument.JfxInstrument;
 
@@ -37,10 +51,11 @@ import org.malai.javafx.instrument.JfxInstrument;
  * @author Arnaud BLOUIN
  */
 public class CodeInserter extends JfxInstrument implements Initializable {
-	@FXML private WebView label;
+	@FXML private TextArea label;
 	@FXML private Button ok;
 	@FXML private Button cancel;
 	@FXML private TextArea text;
+	@FXML private TextArea errorLog;
 	private Stage codeInserterDialogue;
 	@Inject private IDrawing drawing;
 	@Inject private StatusBarController statusBar;
@@ -54,7 +69,68 @@ public class CodeInserter extends JfxInstrument implements Initializable {
 
 	@Override
 	public void initialize(final URL location, final ResourceBundle resources) {
-		label.getEngine().loadContent(LangTool.INSTANCE.getBundle().getString("LaTeXDrawFrame.16"));
+		label.setText(LangTool.INSTANCE.getBundle().getString("LaTeXDrawFrame.16"));
+
+		// Collecting errors from the parser.
+		final ANTLRErrorListener errorListener = new BaseErrorListener() {
+			@Override
+			public void syntaxError(final Recognizer<?, ?> recognizer, final Object offendingSymbol, final int line, final int charPositionInLine,
+									final String msg, final RecognitionException e) {
+				System.out.println(msg);
+				errorLog.setText(errorLog.getText() + "Syntax error: " + msg + LSystem.EOL);
+			}
+		};
+
+		final PSTLatexdrawListener listener = new PSTLatexdrawListener() {
+			@Override
+			public void exitUnknowncmds(final PSTParser.UnknowncmdsContext ctx) {
+				errorLog.setText(errorLog.getText() + "Unknown command: " + ctx.LATEXCMD().getSymbol().getText() + LSystem.EOL);
+			}
+
+			@Override
+			public void enterUnknownParamSetting(final PSTParser.UnknownParamSettingContext ctx) {
+				errorLog.setText(errorLog.getText() + "Unknown parameter: " + ctx.name.getText() + LSystem.EOL);
+			}
+
+			@Override
+			public void visitErrorNode(final ErrorNode node) {
+				errorLog.setText(errorLog.getText() + "Error: " + node.getText() + LSystem.EOL);
+			}
+
+			@Override
+			public void exitText(final PSTParser.TextContext ctx) {
+				super.exitText(ctx);
+				if(ctx.getText().startsWith("\\")) {
+					errorLog.setText(errorLog.getText() + "Bad command: '" + ctx.getText() + "'?" + LSystem.EOL);
+				}
+			}
+		};
+
+		listener.LOG.addHandler(new Handler() {
+			@Override
+			public void publish(final LogRecord record) {
+				errorLog.setText(errorLog.getText() + record.getMessage() + LSystem.EOL);
+			}
+
+			@Override
+			public void flush() {
+			}
+
+			@Override
+			public void close() throws SecurityException {
+			}
+		});
+
+		// Each time a key is pressed, the code is parsed and errors reported.
+		text.addEventHandler(KeyEvent.KEY_PRESSED, evt -> {
+			errorLog.setText("");
+			final PSTLexer lexer = new PSTLexer(CharStreams.fromString(text.getText()));
+			lexer.addErrorListener(errorListener);
+			final PSTParser parser = new PSTParser(new CommonTokenStream(lexer));
+			parser.addParseListener(listener);
+			parser.addErrorListener(errorListener);
+			parser.pstCode(new PSTContext());
+		});
 	}
 
 
@@ -62,7 +138,9 @@ public class CodeInserter extends JfxInstrument implements Initializable {
 	private Stage getInsertCodeDialogue() {
 		if(codeInserterDialogue == null) {
 			try {
-				Parent root = FXMLLoader.load(getClass().getResource("/fxml/InsertCode.fxml"), LangTool.INSTANCE.getBundle(),
+				// The FXML file only loaded only when this method is called: this JFX controller is created by
+				// Guice and lives as a singleton. A call to this function loads the FXML.
+				final Parent root = FXMLLoader.load(getClass().getResource("/fxml/InsertCode.fxml"), LangTool.INSTANCE.getBundle(),
 					new JavaFXBuilderFactory(), LaTeXDraw.getINSTANCE().getInstanceCallBack());
 				final Scene scene = new Scene(root);
 				codeInserterDialogue = new Stage(StageStyle.UTILITY);
@@ -70,7 +148,6 @@ public class CodeInserter extends JfxInstrument implements Initializable {
 				codeInserterDialogue.setScene(scene);
 				codeInserterDialogue.setOnHiding(evt -> setActivated(false));
 			}catch(final Exception ex) {
-				ex.printStackTrace();
 				BadaboomCollector.INSTANCE.add(ex);
 			}
 		}
@@ -80,9 +157,9 @@ public class CodeInserter extends JfxInstrument implements Initializable {
 	@Override
 	public void configureBindings() throws IllegalAccessException, InstantiationException {
 		bindButton(InsertPSTCode.class, action -> {
-			 action.setDrawing(drawing);
-			 action.setCode(text.getText());
-			 action.setStatusBar(statusBar.getLabel());
+			action.setDrawing(drawing);
+			action.setCode(text.getText());
+			action.setStatusBar(statusBar.getLabel());
 		}, ok);
 		bindButton(InactivateInstrument.class, action -> action.setInstrument(this), cancel, ok);
 	}
