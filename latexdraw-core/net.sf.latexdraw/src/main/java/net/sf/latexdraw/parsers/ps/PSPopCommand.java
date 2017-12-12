bfee4fc99497c19690cff8abdac69b56c457c74e
arnobl <blouin.arno@gmail.com>
2017-12-12 20:49:26 +0100

    fix(smell): small code smells removed

--- latexdraw-core/net.sf.latexdraw/src/main/java/net/sf/latexdraw/parsers/ps/PSPopCommand.java
+++ latexdraw-core/net.sf.latexdraw/src/main/java/net/sf/latexdraw/parsers/ps/PSPopCommand.java
@@ -19,7 +19,9 @@ import java.util.Deque;
 public class PSPopCommand implements PSArithemticCommand {
 	@Override
 	public void execute(final Deque<Double> stack, final double x) {
-		if(stack.size() < 1) throw new InvalidFormatPSFunctionException();
+		if(!stack.isEmpty()) {
+			throw new InvalidFormatPSFunctionException();
+		}
 
 		stack.pop();
 	}
