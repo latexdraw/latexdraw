71e1690a680297cf0bbf290e557a6e2e27cfa0d8
arnobl <blouin.arno@gmail.com>
2018-01-08 10:49:27 +0100

    improv(binding): use the binding api

--- latexdraw-core/net.sf.latexdraw/src/main/java/net/sf/latexdraw/instruments/TemplateManager.java
+++ latexdraw-core/net.sf.latexdraw/src/main/java/net/sf/latexdraw/instruments/TemplateManager.java
@@ -12,7 +12,6 @@ package net.sf.latexdraw.instruments;
 
 import java.io.File;
 import java.net.URL;
-import java.util.Collections;
 import java.util.ResourceBundle;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
@@ -35,7 +34,6 @@ import net.sf.latexdraw.models.interfaces.shape.IDrawing;
 import net.sf.latexdraw.util.Inject;
 import net.sf.latexdraw.view.jfx.Canvas;
 import net.sf.latexdraw.view.svg.SVGDocumentGenerator;
-import org.malai.javafx.binding.JfXWidgetBinding;
 import org.malai.javafx.instrument.JfxInstrument;
 import org.malai.javafx.interaction.library.DnD;
 
@@ -94,43 +92,27 @@ public class TemplateManager extends JfxInstrument implements Initializable {
 			action.setTemplatesPane(templatePane);
 			action.updateThumbnails(true);
 		}).bind();
-		addBinding(new DnD2AddTemplate(this));
-	}
 
-
-	private static class DnD2AddTemplate extends JfXWidgetBinding<LoadTemplate, DnD, TemplateManager> {
-		DnD2AddTemplate(final TemplateManager ins) throws InstantiationException, IllegalAccessException {
-			super(ins, false, LoadTemplate.class, new DnD(), Collections.singletonList(ins.templatePane), false, null);
-		}
-
-		@Override
-		public void initAction() {
-			action.setDrawing(instrument.drawing);
-			action.setFile(new File((String) interaction.getSrcObject().get().getUserData()));
-			action.setOpenSaveManager(SVGDocumentGenerator.INSTANCE);
-			action.setProgressBar(instrument.statusController.getProgressBar());
-			action.setStatusWidget(instrument.statusController.getLabel());
-			action.setUi(LaTeXDraw.getInstance());
-		}
-
-		@Override
-		public void updateAction() {
-			final Node srcObj = interaction.getSrcObject().get();
-			final Point3D pt3d = interaction.getEndObjet().get().sceneToLocal(srcObj.localToScene(interaction.getEndLocalPt())).
-				subtract(Canvas.ORIGIN.getX() + srcObj.getLayoutX(), Canvas.ORIGIN.getY() + srcObj.getLayoutY(), 0d);
-			action.setPosition(ShapeFactory.INST.createPoint(pt3d));
-		}
-
-		@Override
-		public void interimFeedback() {
-			instrument.templatePane.setCursor(Cursor.CLOSED_HAND);
-		}
-
-		@Override
-		public boolean isConditionRespected() {
-			return interaction.getSrcObject().orElse(null) instanceof ImageView &&
-				interaction.getSrcObject().get().getUserData() instanceof String &&
-				interaction.getEndObjet().orElse(null) instanceof Canvas;
-		}
+		nodeBinder(LoadTemplate.class, new DnD()).
+			on(templatePane).
+			first((a, i) -> {
+				a.setDrawing(drawing);
+				a.setFile(new File((String) i.getSrcObject().get().getUserData()));
+				a.setOpenSaveManager(SVGDocumentGenerator.INSTANCE);
+				a.setProgressBar(statusController.getProgressBar());
+				a.setStatusWidget(statusController.getLabel());
+				a.setUi(LaTeXDraw.getInstance());
+			}).
+			then((a, i) -> {
+				final Node srcObj = i.getSrcObject().get();
+				final Point3D pt3d = i.getEndObjet().get().sceneToLocal(srcObj.localToScene(i.getEndLocalPt())).
+					subtract(Canvas.ORIGIN.getX() + srcObj.getLayoutX(), Canvas.ORIGIN.getY() + srcObj.getLayoutY(), 0d);
+				a.setPosition(ShapeFactory.INST.createPoint(pt3d));
+			}).
+			feedback(() -> templatePane.setCursor(Cursor.CLOSED_HAND)).
+			when(i -> i.getSrcObject().orElse(null) instanceof ImageView &&
+				i.getSrcObject().get().getUserData() instanceof String &&
+				i.getEndObjet().orElse(null) instanceof Canvas).
+			bind();
 	}
 }
