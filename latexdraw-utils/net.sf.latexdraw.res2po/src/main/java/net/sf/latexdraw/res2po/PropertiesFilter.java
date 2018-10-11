51fe2c9960153d7332dddf6322723cd87d540ddc
arnobl <blouin.arno@gmail.com>
2018-10-11 18:15:06 +0200

    moved related projects to other repositories

--- latexdraw-utils/net.sf.latexdraw.res2po/src/main/java/net/sf/latexdraw/res2po/PropertiesFilter.java
+++
@@ -1,9 +0,0 @@
-package net.sf.latexdraw.res2po;
-import java.io.*;
-
-public class PropertiesFilter implements FileFilter  {
-	@Override
-	public boolean accept(final File pathname) {
-		return pathname != null && pathname.getPath().endsWith(".properties");
-	}
-}
