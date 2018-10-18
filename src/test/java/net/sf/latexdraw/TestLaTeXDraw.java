44a4585b9425618516f68be26fbaa60362a2d00a
arnobl <blouin.arno@gmail.com>
2018-10-18 15:09:04 +0200

    test(ui): reactivation of some UI tests

--- src/test/java/net/sf/latexdraw/TestLaTeXDraw.java
+++ src/test/java/net/sf/latexdraw/TestLaTeXDraw.java
@@ -2,23 +2,31 @@ package net.sf.latexdraw;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import javafx.application.Platform;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseButton;
 import net.sf.latexdraw.badaboom.BadaboomCollector;
 import net.sf.latexdraw.util.Page;
 import net.sf.latexdraw.view.jfx.Canvas;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.malai.command.CommandsRegistry;
+import org.testfx.api.FxRobot;
 import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.util.WaitForAsyncUtils;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 
-//@ExtendWith(ApplicationExtension.class)
+@ExtendWith(ApplicationExtension.class)
 public class TestLaTeXDraw {
 	LaTeXDraw app;
 
@@ -89,16 +97,22 @@ public class TestLaTeXDraw {
 		assertTrue(BadaboomCollector.INSTANCE.isEmpty());
 	}
 
-	//	@Test
-	//	public void testMoveViewPort() {
-	//		Platform.runLater(() -> app.getMainStage().showAndWait());
-	//		WaitForAsyncUtils.waitForFxEvents();
-	//		final ScrollPane pane = lookup("#scrollPane").query();
-	//		final double hvalue = pane.getHvalue();
-	//		final double vvalue = pane.getVvalue();
-	//		drag("#canvas", MouseButton.MIDDLE).dropBy(100d, 200d);
-	//		WaitForAsyncUtils.waitForFxEvents();
-	//		assertThat(hvalue, Matchers.greaterThan(pane.getHvalue()));
-	//		assertThat(vvalue, Matchers.greaterThan(pane.getVvalue()));
-	//	}
+	@Test
+	public void testMoveViewPort(final FxRobot robot) {
+		Platform.runLater(() -> {
+			try {
+				app.getMainStage().showAndWait();
+			}catch(final IllegalStateException ignored) {
+				// Already visible
+			}
+		});
+		WaitForAsyncUtils.waitForFxEvents();
+		final ScrollPane pane = robot.lookup("#scrollPane").query();
+		final double hvalue = pane.getHvalue();
+		final double vvalue = pane.getVvalue();
+		robot.drag("#canvas", MouseButton.MIDDLE).dropBy(100d, 200d);
+		WaitForAsyncUtils.waitForFxEvents();
+		assertThat(hvalue, Matchers.greaterThan(pane.getHvalue()));
+		assertThat(vvalue, Matchers.greaterThan(pane.getVvalue()));
+	}
 }
